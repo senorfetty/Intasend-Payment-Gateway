5d2e21880d6c50b1920d3de1549e5393fb0e51d1
senorfetty <billykimbett@gmail.com>
2025-02-20 20:35:27 +0300

    updated payload

--- payments/views.py
+++ payments/views.py
@@ -1,31 +1,37 @@
 from django.shortcuts import render
 from intasend import APIService
-import os
+import json
+from django.http import Jsonresponse
 from decouple import config
+
 
 token=config('token')
 publishable_key=config('publishable_key')
 service= APIService(token=token,publishable_key=publishable_key,test=True)
 
-payload = {
-    'amount':'',
-    'currency':'',
-    'email':'',
-    'phone_number':'',
-    'first_name':'',
-    'last_name':'',
-    'api_ref':'',
-    'redirect_url':''
-}
 
-checkout = service.checkout.create(data=payload)
+def generate_checkout(request):
+    if request.method == 'POST':
+        try :
+            data= json.loads(request.body)
 
-if checkout['status'] == 'success':
-    checkout_url = checkout['data']['url']
-    print(checkout_url)
+            payload = {
+                'amount': data.get('amount'),
+                'currency': data.get('currency'),
+                'email': data.get('email'),
+                'phone_number': data.get('phone_number'),
+                'first_name': data.get('first_name'),
+                'last_name': data.get('last_name'),
+                'api_ref': data.get('api_ref'),
+                'redirect_url': data.get('redirect_url')
+            }
+            checkout = service.checkout.create(data=payload)
 
-else:
-    print(checkout)
-
-def index(request):
-    return render(request, 'index.html')
+            if checkout['status'] == 'success':
+                return Jsonresponse({'checkout_url' : checkout['data']['url']})
+            
+            else:
+                return Jsonresponse({'error' : checkout.get('message', 'Failed')})
+        except Exception as e:
+            return Jsonresponse({'error': str(e)},status =400)
+    return Jsonresponse({'error': 'Invalid'}, status=405)
