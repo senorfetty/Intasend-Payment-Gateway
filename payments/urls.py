7b65591d3c77bc11c5df381a85b4affe27e2857c
senorfetty <billykimbett@gmail.com>
2025-03-05 17:37:49 +0300

    `Updated IntaSend SDK to version 4.0.7, modified payment button and updated views to use new SDK`

--- payments/urls.py
+++ payments/urls.py
@@ -2,5 +2,7 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('generate_checkout', views.generate_checkout, name='generate_checkout'),
+    # path('generate_checkout', views.generate_checkout, name='generate_checkout'),
+    path('', views.index, name='index')
 ]
+
--- payments/views.py
+++ payments/views.py
@@ -1,37 +1,16 @@
 from django.shortcuts import render
-from intasend import APIService
-import json
-from django.http import Jsonresponse
+
+
+# from intasend import APIService
+# import json
+# from django.http import Jsonresponse
 from decouple import config
 
 
-token=config('token')
-publishable_key=config('publishable_key')
-service= APIService(token=token,publishable_key=publishable_key,test=True)
+# token=config('token')
+# publishable_key=config('publishable_key')
+# service= APIService(token=token,publishable_key=publishable_key,test=True)
 
-
-def generate_checkout(request):
-    if request.method == 'POST':
-        try :
-            data= json.loads(request.body)
-
-            payload = {
-                'amount': data.get('amount'),
-                'currency': data.get('currency'),
-                'email': data.get('email'),
-                'phone_number': data.get('phone_number'),
-                'first_name': data.get('first_name'),
-                'last_name': data.get('last_name'),
-                'api_ref': data.get('api_ref'),
-                'redirect_url': data.get('redirect_url')
-            }
-            checkout = service.checkout.create(data=payload)
-
-            if checkout['status'] == 'success':
-                return Jsonresponse({'checkout_url' : checkout['data']['url']})
-            
-            else:
-                return Jsonresponse({'error' : checkout.get('message', 'Failed')})
-        except Exception as e:
-            return Jsonresponse({'error': str(e)},status =400)
-    return Jsonresponse({'error': 'Invalid'}, status=405)
+def index(request):
+    publicAPIKey = config('publishable_key')
+    return render(request, 'index.html', {'publicAPIKey': publicAPIKey})
